1f2407d3de18d1046b09905b4d739b2317402cff
Anatoliy Basharov <rexonka3@gmail.com>
2024-03-09 14:19:33 +0300

    Finish task PP 2.1.3

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,16 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Timer {
+    @Qualifier("animalCage")
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
-        return nanoTime;
+            return nanoTime;
+
     }
 }
